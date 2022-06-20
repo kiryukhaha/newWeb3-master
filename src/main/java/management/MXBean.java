9aeb0e1ab18710a757606e7ad23291dbe814a7fc
kiryukhaha <84603041+kiryukhaha@users.noreply.github.com>
2022-06-20 21:30:02 +0300

    for vova

---
+++ src/main/java/management/MXBean.java
@@ -0,0 +1,15 @@
+package management;
+
+public interface MXBean {
+    long checkPointAmountDivisor();
+    double getMissPercentage();
+    long getResultAmount();
+    long getMissAmount();
+
+//    void setMissPercentage(double missPercentage);
+//    void setResultAmount(long amount);
+//    void setMissAmount(long amount);
+//    void setSVGResultAmount(long amount)
+
+}
+
--- src/main/java/result/ResultBean.java
+++ src/main/java/result/ResultBean.java
@@ -8,11 +8,15 @@ import java.util.ArrayList;
 
 import javax.management.Notification;
 import javax.management.NotificationBroadcasterSupport;
-
+import management.MXBean;
 @ManagedBean(name = "resultBean")
 @SessionScoped
-public class ResultBean extends NotificationBroadcasterSupport implements Serializable {
-
+public class ResultBean extends NotificationBroadcasterSupport implements Serializable , MXBean{
+    private long sequenceNumber = 0;
+    private long resultAmount = 0;
+    private long missAmount = 0;
+    private long svgAmount = 0;
+    private double missPercentage = 0;
 
     private Result newResult;
 
@@ -60,7 +64,8 @@ public class ResultBean extends NotificationBroadcasterSupport implements Serial
         list.add(finalResult);}
 //        Result resultForTable = new Result(newResult.getX(), newResult.getY(), newResult.getR(), newResult.getResult());
 //        newResult = new Result(0,0,1,false);
-        sendNotification(new Notification("Result amount can be divided by 15", this.getClass().getName(), 15, "Overall number of results: " + 15 + "\n Missed results: " + 15));
+
+        checkPointAmountDivisor();
         return "update";
     }
 
@@ -73,4 +78,39 @@ public class ResultBean extends NotificationBroadcasterSupport implements Serial
         return list;
     }
 
+
+
+    @Override
+    public long getResultAmount() {
+
+        return list.size();
+    }
+
+
+
+
+    @Override
+    public long getMissAmount() {
+
+        return list.stream().filter(result -> result.getResult() == false).count();
+    }
+
+    @Override
+    public long checkPointAmountDivisor() {
+        long amount = getResultAmount();
+        long misses = getMissAmount();
+        resultAmount = amount;
+        missAmount = misses;
+
+            sendNotification(new Notification("Result amount can be divided by 15", this.getClass().getName(), sequenceNumber++, "Overall number of results: " + list.size() + "\n Missed results: " + misses));
+
+        return amount;
+    }
+
+    @Override
+    public double getMissPercentage() {
+
+
+        return 0;
+    }
 }
